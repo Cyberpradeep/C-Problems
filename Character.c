f0941cdcb6564d3b062f0568b80fad4837286008
Pradeep_Selladurai <152625492+Cyberpradeep@users.noreply.github.com>
2025-01-20 20:39:19 +0530

    Create Character.c

---
+++ Character.c
@@ -0,0 +1,17 @@
+//QUESTION:
+// Write a program to get a character value from the user and display it.
+// INPUT & OUTPUT FORMAT:
+// Input consists of 1 character.
+// Output consists of 1 character.
+// SAMPLE INPUT:
+// d
+// SAMPLE OUTPUT:
+// d
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    printf("%c",ch);
+    return 0;
+}
